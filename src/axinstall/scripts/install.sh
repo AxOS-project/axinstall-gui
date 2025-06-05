4e185dfef7f272661483054a3c3fdac19fa3c416
Byson94 <byson94@outlook.com>
2025-06-05 21:33:04 +0530

    nvrm my previous commit was just me bluffing

--- src/axinstall/scripts/install.sh
+++ src/axinstall/scripts/install.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/bash
-set -euo pipefail
+set -uo pipefail
 
-echo "Running reflector to sort for fastest mirrors" | tee -a /tmp/axinstall-output.txt
-pkexec reflector --latest 5 --sort rate --save /etc/pacman.d/mirrorlist | tee -a /tmp/axinstall-output.txt
-echo "Initializing pacman keyring" | tee -a /tmp/axinstall-output.txt
-pkexec pacman-key --init | tee -a /tmp/axinstall-output.txt
-pkexec pacman-key --populate archlinux | tee -a /tmp/axinstall-output.txt
-echo "Starting installation" | tee -a /tmp/axinstall-output.txt
-pkexec axinstall-cli config ~/.config/axinstall.json | tee -a /tmp/axinstall-output.txt
+logfile="/tmp/axinstall-output.txt"
+echo "Running reflector to sort for fastest mirrors" | tee -a "$logfile"
+if ! pkexec reflector --latest 5 --sort rate --save /etc/pacman.d/mirrorlist | tee -a "$logfile"; then
+    echo "Warning: Reflector failed, continuing with default mirrors" | tee -a "$logfile"
+fi
+
+set -e # Now we can exit because now we are moving to critical stuff
+
+echo "Initializing pacman keyring" | tee -a "$logfile"
+pkexec pacman-key --init | tee -a "$logfile"
+pkexec pacman-key --populate archlinux | tee -a "$logfile"
+
+echo "Starting installation" | tee -a "$logfile"
+pkexec axinstall-cli config ~/.config/axinstall.json | tee -a "$logfile"
